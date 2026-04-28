d28657cb93f0df7083dbefaea862d807c5dc475c
Amit Pal <palamit9411@gmail.com>
2026-04-28 14:43:58 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -1,29 +1,51 @@
 import streamlit as st
 
+def footer(text_color="white"):
+    col1, col2, col3 = st.columns([3, 2, 3])
 
+    with col2:
+        st.markdown(
+            f"""
+            <div style="text-align:center;">
+                <p style="font-weight:600; color:{text_color}; margin-bottom:5px;">
+                    Created with 💖 by
+                </p>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+        st.image("src/components/footer_icon.png", width=70)
 
 def footer_home():
-    st.image("src/components/footer_icon.png", width=80)
+    footer("white")
 
-    # logo_url = "src/components/footer_icon.png"
-    # st.markdown(f"""
-    #     <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
-    #         <p style="font-weight:bold; color:white;">Created with 💖 by </p>
-    #         <img src='{logo_url}' style='max-height:25px' />
-    #     </div>
+def footer_dashboard():
+    footer("black")
 
-    #             """, unsafe_allow_html=True)
+
+
+# def footer_home():
+#     st.image("src/components/footer_icon.png", width=80)
+
+#     # logo_url = "src/components/footer_icon.png"
+#     # st.markdown(f"""
+#     #     <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
+#     #         <p style="font-weight:bold; color:white;">Created with 💖 by </p>
+#     #         <img src='{logo_url}' style='max-height:25px' />
+#     #     </div>
+
+#     #             """, unsafe_allow_html=True)
     
 
 
-def footer_dashboard():
-    st.image("src/components/footer_icon.png", width=80)
+# def footer_dashboard():
+#     st.image("src/components/footer_icon.png", width=80)
 
-    # logo_url = "src/components/footer_icon.png"
-    # st.markdown(f"""
-    #     <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
-    #         <p style="font-weight:bold; color:black;">Created with 💖 by </p>
-    #         <img src='{logo_url}' style='max-height:25px' />
-    #     </div>
+#     # logo_url = "src/components/footer_icon.png"
+#     # st.markdown(f"""
+#     #     <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
+#     #         <p style="font-weight:bold; color:black;">Created with 💖 by </p>
+#     #         <img src='{logo_url}' style='max-height:25px' />
+#     #     </div>
 
-    #             """, unsafe_allow_html=True)
+#     #             """, unsafe_allow_html=True)
