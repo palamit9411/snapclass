3883512f92e15400a3e35f45578074abc0763cc3
Amit Pal <palamit9411@gmail.com>
2026-04-28 14:38:17 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -4,9 +4,9 @@ import streamlit as st
 
 def footer_home():
 
-    logo_url = "footer_icon.png"
+    logo_url = "src/components/footer_icon.png"
     st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
             <p style="font-weight:bold; color:white;">Created with 💖 by </p>
             <img src='{logo_url}' style='max-height:25px' />
         </div>
@@ -17,9 +17,9 @@ def footer_home():
 
 def footer_dashboard():
 
-    logo_url = "footer_icon.png"
+    logo_url = "src/components/footer_icon.png"
     st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
             <p style="font-weight:bold; color:black;">Created with 💖 by </p>
             <img src='{logo_url}' style='max-height:25px' />
         </div>
