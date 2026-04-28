4510587128639a7a8d99bb9be8ced2dd56fe04af
Amit Pal <palamit9411@gmail.com>
2026-04-28 14:40:33 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -3,25 +3,27 @@ import streamlit as st
 
 
 def footer_home():
+    st.image("src/components/footer_icon.png", width=80)
 
-    logo_url = "src/components/footer_icon.png"
-    st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
-            <p style="font-weight:bold; color:white;">Created with 💖 by </p>
-            <img src='{logo_url}' style='max-height:25px' />
-        </div>
+    # logo_url = "src/components/footer_icon.png"
+    # st.markdown(f"""
+    #     <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
+    #         <p style="font-weight:bold; color:white;">Created with 💖 by </p>
+    #         <img src='{logo_url}' style='max-height:25px' />
+    #     </div>
 
-                """, unsafe_allow_html=True)
+    #             """, unsafe_allow_html=True)
     
 
 
 def footer_dashboard():
+    st.image("src/components/footer_icon.png", width=80)
 
-    logo_url = "src/components/footer_icon.png"
-    st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
-            <p style="font-weight:bold; color:black;">Created with 💖 by </p>
-            <img src='{logo_url}' style='max-height:25px' />
-        </div>
+    # logo_url = "src/components/footer_icon.png"
+    # st.markdown(f"""
+    #     <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; align-items:center">
+    #         <p style="font-weight:bold; color:black;">Created with 💖 by </p>
+    #         <img src='{logo_url}' style='max-height:25px' />
+    #     </div>
 
-                """, unsafe_allow_html=True)
+    #             """, unsafe_allow_html=True)
