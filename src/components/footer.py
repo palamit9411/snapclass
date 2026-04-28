5ab6731c7d9d6cc6c0b5a9c9201b23725311cd23
Amit Pal <palamit9411@gmail.com>
2026-04-28 16:17:12 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -12,12 +12,30 @@ def footer(text_color="white"):
     img_base64 = get_base64_image()
 
     st.html(f"""
-    <div style="margin:20px auto; display:flex; justify-content:center; align-items:center; gap:6px; color:{text_color}; font-weight:700; letter-spacing:0.5px; width:fit-content; padding:6px 12px; border-radius:10px; background-color:#0D0D0D;">
+    <div style="
+        margin:20px auto;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        gap:6px;
+        color:{text_color};
+        font-weight:700;
+        letter-spacing:0.5px;
+        width:fit-content;
+        padding:8px 16px;
+        border-radius:12px;
+        background-color:#0D0D0D;
+    ">
         
-        <span>Created with ❤️ by</span>
+        <span style="font-size:16px;">Created with ❤️ by</span>
         
         <img src="data:image/png;base64,{img_base64}" 
-             style="height:100px; vertical-align:middle; filter: brightness(2) contrast(1.6);" />
+             style="
+                height:70px;
+                transform: scale(1.4);
+                transform-origin: center;
+                filter: brightness(2.2) contrast(1.7);
+             " />
     
     </div>
     """)
