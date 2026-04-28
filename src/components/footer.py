d2aa2653052a43a5a5395df72c545e5e5ba6ba16
Amit Pal <palamit9411@gmail.com>
2026-04-28 21:08:57 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -3,33 +3,18 @@ import streamlit as st
 def footer(text_color="white"):
     logo_url = "https://raw.githubusercontent.com/palamit9411/snapclass/main/src/components/footer_icon.png"
 
-    st.markdown(f"""
-        <div style="
-            margin-top:2rem;
-            display:flex;
-            gap:6px;
-            justify-content:center;
-            align-items:center;
-        ">
-            <p style="
-                font-weight:bold;
-                color:{text_color};
-                margin:0;
-                font-size:1rem;
-            ">
-                Created with ❤️ by
-            </p>
-
-            <img src="{logo_url}" 
-                 style="
-                     height:1.8rem;
-                     width:auto;
-                     vertical-align:middle;
-                     object-fit:contain;
-                 ">
-        </div>
+    st.markdown("""
+    <div style="display:flex; justify-content:center; align-items:center; gap:6px; margin-top:2rem;">
+    
+        <span style="color:white; font-weight:bold; font-size:1rem;">
+            Created with ❤️ by
+        </span>
+    
+        <img src="{logo_url}" 
+             style="height:1.8rem; width:auto; vertical-align:middle; object-fit:contain;" />
+    
+    </div>
     """, unsafe_allow_html=True)
-
 
 def footer_home():
     footer("white")
