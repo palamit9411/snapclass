ff9f32b3e39f8bab9cbca87d96b021ec7db42c7e
Amit Pal <palamit9411@gmail.com>
2026-04-28 17:04:52 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -1,25 +1,31 @@
 import streamlit as st
 
+def footer(text_color="white"):
+    logo_url = "https://raw.githubusercontent.com/palamit9411/snapclass/main/src/components/footer_icon.png"
+
+    st.markdown(f"""
+        <div style="
+            margin-top:2rem;
+            display:flex;
+            gap:8px;
+            justify-content:center;
+            align-items:center;
+        ">
+            <p style="
+                font-weight:600;
+                color:{text_color};
+                margin:0;
+            ">
+                Created with ❤️ by
+            </p>
+            <img src="{logo_url}" style="height:26px;" />
+        </div>
+    """, unsafe_allow_html=True)
+
+
 
 def footer_home():
-    logo_url = "https://raw.githubusercontent.com/palamit9411/snapclass/main/src/components/footer_icon.png"
-    
-    st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
-        <img src='{logo_url}' style='max-height:25px' />
-        </div>
-                
-                """, unsafe_allow_html=True)
-
+    footer("white")
 
 def footer_dashboard():
-    logo_url = "https://raw.githubusercontent.com/palamit9411/snapclass/main/src/components/footer_icon.png"
-    
-    st.markdown(f"""
-        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:black;"> Created with ❤️ by </p>  
-        <img src='{logo_url}' style='max-height:25px' />
-        </div>
-                
-                """, unsafe_allow_html=True)
+    footer("black")
