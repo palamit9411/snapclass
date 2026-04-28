0b18810681bf1d2b8c0c89c574cab65ba3774f55
Amit Pal <palamit9411@gmail.com>
2026-04-28 16:44:57 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -1,32 +1,25 @@
 import streamlit as st
-import base64
-import os
 
-def get_base64_image():
-    base_dir = os.path.dirname(__file__)
-    full_path = os.path.join(base_dir, "footer_icon.png")
-    with open(full_path, "rb") as f:
-        return base64.b64encode(f.read()).decode()
-
-def footer(text_color="white"):
-    img_base64 = get_base64_image()
-
-    st.markdown(
-        f"""
-        <div style="margin:20px auto; display:flex; justify-content:center; align-items:center; gap:6px; color:{text_color}; font-weight:700; letter-spacing:0.5px; width:fit-content; padding:10px 16px; border-radius:12px; background-color:#0D0D0D;">
-            
-            <span>Created with ❤️ by</span>
-            
-            <img src="data:image/png;base64,{img_base64}" 
-                 style="height:60px; mix-blend-mode:lighten;" />
-        
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
 
 def footer_home():
-    footer("white")
+    logo_url = "https://raw.githubusercontent.com/palamit9411/snapclass/main/src/components/footer_icon.png"
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
+        <img src='{logo_url}' style='max-height:25px' />
+        </div>
+                
+                """, unsafe_allow_html=True)
+
 
 def footer_dashboard():
-    footer("white")
+    logo_url = "https://raw.githubusercontent.com/palamit9411/snapclass/main/src/components/footer_icon.png"
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Created with ❤️ by </p>  
+        <img src='{logo_url}' style='max-height:25px' />
+        </div>
+                
+                """, unsafe_allow_html=True)
